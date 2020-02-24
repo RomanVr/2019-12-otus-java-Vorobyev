9578881512af3bbd87b98dbaf9abe55b84f10cb9
RomanVR <romakf99@yandex.ru>
2020-02-24 16:13:37 +0300

    TestAnnotation module created

---
+++ HW05-TestAnnotation/src/main/java/ru/otus/TestAnnotation/DemoTestMain.java
@@ -0,0 +1,9 @@
+package ru.otus.TestAnnotation;
+
+public class DemoTestMain {
+    public static void main(String[] args) {
+        SampleForTests sample = new SampleForTests();
+
+        TestRunner.run(sample);
+    }
+}
